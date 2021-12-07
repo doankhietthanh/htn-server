f28f82b872c56b8180063894cf2ef585e5bb5a87
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2021-12-07 19:22:05 +0700

    ádasd

--- route/getToken.js
+++ route/getToken.js
@@ -1,10 +1,35 @@
 const data = require("../data/data.json");
-
+function removeAccents(str) {
+  var AccentsMap = [
+    "aàảãáạăằẳẵắặâầẩẫấậ",
+    "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+    "dđ",
+    "DĐ",
+    "eèẻẽéẹêềểễếệ",
+    "EÈẺẼÉẸÊỀỂỄẾỆ",
+    "iìỉĩíị",
+    "IÌỈĨÍỊ",
+    "oòỏõóọôồổỗốộơờởỡớợ",
+    "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+    "uùủũúụưừửữứự",
+    "UÙỦŨÚỤƯỪỬỮỨỰ",
+    "yỳỷỹýỵ",
+    "YỲỶỸÝỴ",
+  ];
+  for (var i = 0; i < AccentsMap.length; i++) {
+    var re = new RegExp("[" + AccentsMap[i].substr(1) + "]", "g");
+    var char = AccentsMap[i][0];
+    str = str.replace(re, char);
+  }
+  return str.trim();
+}
 //function getToken: check token with each person, if true => add data.json, false => print message: "user not found"
 const getToken = (req, res) => {
   // console.log(req.body);
   const index = data.findIndex(
-    (item) => item.name == req.body.name && item.studentID == req.body.studentID
+    (item) =>
+      removeAccents(item.name) == removeAccents(req.body.name) &&
+      item.studentID == req.body.studentID
   );
   if (index == -1)
     return res.json({ success: false, message: "user not found" });
